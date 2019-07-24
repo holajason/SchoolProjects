e2171692993e15e4b35a8ec5647329f82fba6ea4
Jason <jasonlu@umich.edu>
2019-07-24 01:52:58 -0400

    Assignment 2

--- CIS400 - Programming Lang/CIS400Q2/Question1.pl
+++ CIS400 - Programming Lang/CIS400Q2/Question1.pl
@@ -1,6 +1,14 @@
 use warnings;
 use strict;
 
+print "Enter IP address\n";
+my $testIP = readline STDIN;
 
-int len1 len2
-cin >>len>>len2
+if($testIP=~/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/ &&
+(($1<=255  && $2<=255 && $3<=255  &&$4<=255 )))
+{
+print "IP address is valid.\n";
+}
+else{
+	print "IP address is not valid.\n";
+ }
